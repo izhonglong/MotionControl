6c2e02c1b01f812249ef6fcc81c9fba00c8d094b
izhonglong <654438502@qq.com>
2017-12-31 16:14:42 +0800

    no message

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 void main(void)
 {
+	int i - 0;
 	for (int i = 0; i < 10; i++)
 	{
 		for (int j = 0; j < 10; j++)
@@ -12,3 +13,33 @@ void main(void)
 	
 	return;
 }
+
+void fun3(void)
+{
+	
+	return;
+}
+
+void f4(void)
+{
+	return;
+}
+
+void fun2(void)
+{
+	int i = 0;
+	
+	return;
+}
+
+void f3(void)
+{
+	int c = 0;
+
+	int i = 0;
+
+	return;
+}
+
+int a = 0;
+
